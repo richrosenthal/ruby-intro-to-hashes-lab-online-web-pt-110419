da12f0a54649660372a70394639f235b8d1160a1
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-03 17:47:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,6 +36,11 @@ def monopoly_with_third_tier
 	  :railroads=> {
 	    :pieces=>4,
 	    :names=>{
+	      :reading_railroad=>{},
+	      :pennsylvania_railroad=>{},
+	      :b_and_o_railroad=>{},
+	      :shortline=>{}
+	      
 	      
 	    },
 	    :rent_in_dollars=> {
